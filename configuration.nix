84d1b499256f8df100b4e0ab0ee153713d2fac0f
Jethro Kuan <jethrokuan95@gmail.com>
2017-04-03 16:56:22 +0800

    Add mbsync, make fish default shell

--- configuration.nix
+++ configuration.nix
@@ -65,8 +65,32 @@
     emacs
   ];
 
+  systemd.user = { 
+    services = {
+      mbsync = {
+        description = "mailbox sync";
+        after       = [ "network-online.target" ];
+        wantedBy    = [ "default.target" ];
+        path        = [ pkgs.isync ];
+        script      = ''mbsync -a'';
+      };
+    };
+
+    timers = {
+      mbsync = {
+        timerConfig = {
+          OnUnitInactiveSec = "5min";
+          Persistent = "true";
+        };
+        wantedBy = [ "timers.target" ];
+      };
+    };
+
+  };
+
   programs.java.enable = true;
   programs.fish.enable = true;
+  users.defaultUserShell = "/run/current-system/sw/bin/fish";
 
   fonts = {
     enableFontDir = true;
