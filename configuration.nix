50ecbb7af315405263551da5fdd6707de5220a80
Jethro Kuan <jethrokuan95@gmail.com>
2017-04-11 15:01:36 +0800

    remove some fonts, add sane defaults

--- configuration.nix
+++ configuration.nix
@@ -101,13 +101,23 @@
     enableFontDir = true;
     enableGhostscriptFonts = true;
     fonts = with pkgs; [
-      freefont_ttf
-      liberation_ttf
-      ubuntu_font_family
-      dejavu_fonts
-      vistafonts
+      corefonts
       iosevka
+      source-code-pro
+      source-sans-pro
+      source-serif-pro
     ];
+
+    fontconfig = {
+      defaultFonts = {
+       monospace = [ "Source Code Pro" ];
+       sansSerif = [ "Source Sans Pro" ];
+       serif = [ "Source Serif Pro" ];
+      };
+      ultimate = {
+        enable = false;
+      };
+    };
   };
 
   #Services
