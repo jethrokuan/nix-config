17388d6a611352c23276eedb1baa75d888eeb515
Jethro Kuan <jethrokuan95@gmail.com>
2017-06-16 01:55:54 +0800

    use bash as default

--- modules/shells.nix
+++ modules/shells.nix
@@ -8,15 +8,12 @@
     urxvt_tabbedex
 
     # shells
-    fish
     bash
   ];
 
   environment.shells = [
-    "${pkgs.fish}/bin/fish"
     "${pkgs.bash}/bin/bash"
   ];
   
-  programs.fish.enable = true;
-  users.defaultUserShell = "/run/current-system/sw/bin/fish";
+  users.defaultUserShell = "/run/current-system/sw/bin/bash";
 }
