30d5c04d963e2e4bf6b714c29ca2b881b1c097ac
Jethro Kuan <jethrokuan95@gmail.com>
2017-06-15 21:37:52 +0800

    Enable pulseaudio on desktop

--- desktop/extra.nix
+++ desktop/extra.nix
@@ -8,11 +8,7 @@
     install rt2800usb ${pkgs.kmod}/sbin/modprobe --ignore-install rt2800usb && echo 0x0B05 0x17E8 >/sys/bus/usb/drivers/rt2800usb/new_id
   '';
 
-  sound.extraConfig =
-    ''
-      defaults.pcm.!card 1
-      defaults.ctl.!card 1
-    '';
+  hardware.pulseaudio.enable = true;
 
   networking.hostName = "jethro-desktop"; # Define your hostname.
 
