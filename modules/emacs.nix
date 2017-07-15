a7b5dfb9a4ba32c8e451123e66b8b474cdf1c9a0
Jethro Kuan <jethrokuan95@gmail.com>
2017-07-15 20:40:10 +0800

    Add emacs python packages

--- modules/emacs.nix
+++ modules/emacs.nix
@@ -1,8 +1,14 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = [
-    pkgs.emacs
+  environment.systemPackages = with pkgs; [
+    emacs
+
+    # Python packages for dev
+    python27Full
+    python27Packages.flake8
+    python27Packages.isort
+    python27Packages.yapf
   ];
 
   environment.variables.EDITOR = pkgs.lib.mkOverride 0 "emacs";
