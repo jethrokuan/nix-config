2d792cc3166588c9a681b9711126d073bccad864
Jethro Kuan <jethrokuan95@gmail.com>
2017-11-13 19:15:28 +0800

    remove urxvt

--- modules/shells.nix
+++ modules/shells.nix
@@ -1,12 +1,6 @@
 { config, pkgs, ...}:
 {
   environment.systemPackages = with pkgs; [
-    # urxvt
-    rxvt_unicode-with-plugins
-    urxvt_font_size
-    urxvt_perl
-    # urxvt_tabbedex
-
     # shells
     bash
     fish
@@ -20,6 +14,4 @@
   programs.fish.enable = true;
   
   users.defaultUserShell = "/run/current-system/sw/bin/fish";
-
-  services.urxvtd.enable = true;
 }
