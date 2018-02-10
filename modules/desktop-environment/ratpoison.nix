398dd9d3a58356b0b5ca7f7fdf7d79782fef1d3e
Jethro Kuan <jethrokuan95@gmail.com>
2018-02-10 20:29:00 +0800

    remove conky and dzen2

--- modules/desktop-environment/ratpoison.nix
+++ modules/desktop-environment/ratpoison.nix
@@ -1,13 +1,12 @@
 { config, pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    dzen2
-    conky
     nitrogen
   ];
 
   services.xserver.desktopManager.gnome3.enable =true;
-  services.xserver.desktopManager.default = "gnome3";
   services.xserver.windowManager.ratpoison.enable = true;
+
+  services.xserver.desktopManager.default = "gnome3";
   services.xserver.windowManager.default = "ratpoison";
 }
