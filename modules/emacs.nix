3926226b16596219566269f61b84be61559b7be7
Jethro Kuan <jethrokuan95@gmail.com>
2018-04-11 18:13:48 +0800

    Use emacs 26

--- modules/emacs.nix
+++ modules/emacs.nix
@@ -1,8 +1,15 @@
 { pkgs, ... }:
 
 let
-  myEmacs = pkgs.emacs;
-  emacsWithPackages = (pkgs.emacsPackagesNgGen myEmacs).emacsWithPackages;
+  emacs26 = pkgs.stdenv.lib.overrideDerivation pkgs.emacs (oldAttrs : {
+    version = "26.1RC1";
+    src = pkgs.fetchurl {
+      url = "ftp://alpha.gnu.org/gnu/emacs/pretest/emacs-26.1-rc1.tar.xz";
+      sha256 = "6594e668de00b96e73ad4f168c897fe4bca7c55a4caf19ee20eac54b62a05758";
+    };
+    patches = [];
+  });
+  emacsWithPackages = (pkgs.emacsPackagesNgGen emacs26).emacsWithPackages;
   inherit (import ./emacs-packages.nix {inherit pkgs;}) myPackages;
   emacs = emacsWithPackages myPackages;
 in
