7d61b263e2d9cc4dfcf7f2d0e24397f1785d078e
Jethro Kuan <jethrokuan95@gmail.com>
2018-08-11 10:12:47 +0800

    upgrade system.nixos.stateVersion -> system.stateVersion

--- base.nix
+++ base.nix
@@ -77,7 +77,7 @@
     };
 
   # The NixOS release to be compatible with for stateful data such as databases.
-  system.nixos.stateVersion = "18.03";
+  system.stateVersion = "18.03";
 
   system.autoUpgrade = {
     enable = true;
