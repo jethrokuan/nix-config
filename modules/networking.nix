9b8a11334ba59a45253bec37273fe25b1b52452d
Jethro Kuan <jethrokuan95@gmail.com>
2018-09-16 12:29:47 +0800

    update hosts file

--- modules/networking.nix
+++ modules/networking.nix
@@ -1,5 +1,8 @@
 {pkgs, ...}:
 
+let
+  hostsVersion = "1553048103e57638ae653843ae1b8258ece1423a";
+in
 {
   networking.networkmanager.enable = true;
 
@@ -9,7 +12,7 @@
   ];
 
   networking.extraHosts = builtins.readFile (pkgs.fetchurl {
-    url = https://github.com/StevenBlack/hosts/raw/aa60fb8642ae67da35f3004cd2a6dbe9c64ce9fe/alternates/fakenews-gambling-porn-social/hosts;
-    sha256 = "1rf4823nkg95r1hlrhgfash1p0g7lz8c9zfgs5axqbjh1jqqq9w3";
+    url = "https://github.com/StevenBlack/hosts/raw/${hostsVersion}/alternates/fakenews-gambling-porn-social/hosts";
+    sha256 = "1x6x6ak8zamk0jbcfgf8bwqbn87vqdp0nnk51mxx6lh9s69v4dk1";
   });
 }
