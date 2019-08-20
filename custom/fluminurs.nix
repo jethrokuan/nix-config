50bb4e920ca8e795693834b96636ca9db9659707
Jethro Kuan <jethrokuan95@gmail.com>
2019-08-20 08:29:16 +0800

    Bump fluminurs to 0.2.0

--- custom/fluminurs.nix
+++ custom/fluminurs.nix
@@ -1,9 +1,9 @@
-{ pkgs, ... }:
+{ pkgs ? import <nixpkgs> {} }:
 
 with pkgs;
 rustPlatform.buildRustPackage rec {
   name = "fluminurs-${version}";
-  version = "0.1.6";
+  version = "0.2.0";
 
   nativeBuildInputs = [
     rustc cargo
@@ -17,17 +17,17 @@ rustPlatform.buildRustPackage rec {
   src = fetchFromGitHub {
     owner = "indocomsoft";
     repo = "fluminurs";
-    rev = "8b22c761de0f762ce430fb67e7a227c2f0f3fe62";
-    sha256 = "1alvrvbqnyqdb2nlfw5752dgzfiwsh0cj53q9pb3sfagvscdsbzv";
-    # date = 2019-03-07T10:57:44+08:00;
+    rev = "df34b1e92f52ef796ee5eaab7ce6c80440e1aabe";
+    sha256 = "1g9xg9cljbn8iprv3ab33xmzvjd1kz21kzwqy502npk6qdvav99f";
+    # date = 2019-08-15T22:26:14-07:00;
   };
 
-  cargoSha256 = "0drgjnp8wvv0ap6l5x2ss88yp2lhli3wkf56gm0fbnyqq6s2ccpc";
+  cargoSha256 = "14vs59k7mm3krdzggkvijnqpyjs1p9f5g5aqgsgrsdqhp385yz6j";
 
   meta = with stdenv.lib; {
     description = "Luminus api";
     homepage = https://github.com/indocomsoft/fluminurs;
-    license = licenses.unlicense;
+    license = licenses.mit;
     platforms = platforms.all;
   };
 }
