6f7a901c9325ce463736c638d070c5f3d9cf484b
Jethro Kuan <jethrokuan95@gmail.com>
2019-08-27 09:42:37 +0800

    Bump fluminurs for api endpoint fix

--- custom/fluminurs.nix
+++ custom/fluminurs.nix
@@ -17,12 +17,12 @@ rustPlatform.buildRustPackage rec {
   src = fetchFromGitHub {
     owner = "indocomsoft";
     repo = "fluminurs";
-    rev = "df34b1e92f52ef796ee5eaab7ce6c80440e1aabe";
-    sha256 = "1g9xg9cljbn8iprv3ab33xmzvjd1kz21kzwqy502npk6qdvav99f";
-    # date = 2019-08-15T22:26:14-07:00;
+    rev = "fc8aca19699f86fc5d14ecabb7ba0af4c13f2da5";
+    sha256 = "10wfjbznc2mfyb663bqxb6kay9qpspkjd2r57c4dfkmqmrmfviib";
+    # date = 2019-08-27T07:53:38+08:00;
   };
 
-  cargoSha256 = "14vs59k7mm3krdzggkvijnqpyjs1p9f5g5aqgsgrsdqhp385yz6j";
+  cargoSha256 = "0ngakh54kq8rikvxs5nfy5gw9hpawwdy6j9g6gvpkfcv55k4amr9";
 
   meta = with stdenv.lib; {
     description = "Luminus api";
