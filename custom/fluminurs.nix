df2e2c733fd01a3205dcf14f0691d04e26fd588d
Jethro Kuan <jethrokuan95@gmail.com>
2020-01-21 02:37:23 +0800

    bump fluminurs to pull in fix

--- custom/fluminurs.nix
+++ custom/fluminurs.nix
@@ -3,7 +3,7 @@
 with pkgs;
 rustPlatform.buildRustPackage rec {
   name = "fluminurs-${version}";
-  version = "1.1.3";
+  version = "1.1.4";
 
   nativeBuildInputs = [
     rustc cargo
@@ -17,11 +17,11 @@ rustPlatform.buildRustPackage rec {
   src = fetchFromGitHub {
     owner = "indocomsoft";
     repo = "fluminurs";
-    rev = "e7313c3ef57e955c0181e1d2323343154af7e0c7";
-    sha256 = "1fax923f2ygs3vdjggw6fw1f7vwk4a9lf30v49pqj9111j6fpjjb";
+    rev = version;
+    sha256 = "1m3r6pmz95syw33avpkkvnd3wxl72jzb14cr9r573v0jbya0bgv7";
   };
 
-  cargoSha256 = "1296zw7r6m1gz5cgk33qrdrdnnw1xsfv75chnmya0i24chr9cvby";
+  cargoSha256 = "0nrh9n3vpc7dji4bbslhyyqlj458dljhmhb0w3j4kcng1gg1723z";
 
   meta = with stdenv.lib; {
     description = "Luminus api";
