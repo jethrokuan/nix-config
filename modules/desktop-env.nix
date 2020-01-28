2a92fa8d2f5eca8db7675362c780b4f24551ea0b
Jethro Kuan <jethrokuan95@gmail.com>
2020-01-28 20:17:50 +0800

    Add libvterm

--- modules/desktop-env.nix
+++ modules/desktop-env.nix
@@ -1,7 +1,12 @@
 { config, pkgs, ... }:
 
 {
-  services.xserver.windowManager.exwm.enable = true;
+  services.xserver.windowManager.exwm = {
+    enable = true;
+    extraPackages = epkgs: [
+      epkgs.emacs-libvterm
+    ];
+  };
   environment.systemPackages = with pkgs; [
     numix-cursor-theme
   ];
