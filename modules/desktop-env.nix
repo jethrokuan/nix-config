eab6ca08369a5330a573f45c0c2c3ab1dd2d3834
Jethro Kuan <jethrokuan95@gmail.com>
2020-06-03 13:53:13 +0800

    add org-protocol desktop item

--- modules/desktop-env.nix
+++ modules/desktop-env.nix
@@ -20,5 +20,14 @@ in {
        fd
        clang
        texlive.combined.scheme-full
+
+       (makeDesktopItem {
+         name = "org-protocol";
+         exec = "emacsclient %u";
+         comment = "Org Protocol";
+         desktopName = "org-protocol";
+         type = "Application";
+         mimeType = "x-scheme-handler/org-protocol";
+       })
      ]));
 }
