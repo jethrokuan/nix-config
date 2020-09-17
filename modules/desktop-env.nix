d7dd35cd200049eb3deca1f20a9fc066ba1be1a4
Jethro Kuan <jethrokuan95@gmail.com>
2020-09-17 15:11:11 +0800

    add screenshotting tool

--- modules/desktop-env.nix
+++ modules/desktop-env.nix
@@ -3,4 +3,8 @@
   services.xserver.enable = true;
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    kdeApplications.spectacle
+  ];
 }
